4e5b2b70b28dcbf079f66bef8d621ca96248f2b2
sastiam <sfranciach@gmail.com>
2021-07-04 15:18:52 -0500

    WrapperSkills responsivo

--- src/components/Skills/WrapperSkills.js
+++ src/components/Skills/WrapperSkills.js
@@ -1,11 +1,28 @@
-import styled from 'styled-components';
+import styled from "styled-components";
+import breakpoint from "../../common/breakpoint";
 
 const WrapperSkills = styled.div`
+  @media only screen and ${breakpoint.device.xs} {
+    background-color: #2d3436;
+    display: grid;
+    grid-template-columns: repeat(1, 1fr);
+    // height: 450px;
+    padding: 0.5rem;
+  }
+  @media only screen and ${breakpoint.device.sm} {
+    background-color: #2d3436;
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    // height: 450px;
+    padding: 0.5rem;
+  }
+  @media only screen and ${breakpoint.device.lg} {
     background-color: #2d3436;
     display: grid;
     grid-template-columns: repeat(6, 1fr);
     height: 450px;
-    padding: .5rem;
+    padding: 0.5rem;
+  }
 `;
 
 export default WrapperSkills;
